2c107c2fd1e8278afcd9c54f3f3fa85f3abc5914
sujeerap <sujeer@gmail.com>
2026-04-30 16:53:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/send-email', async (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
-      port: parseInt(process.env.SMTP_PORT || '587'),
+      port: parseInt(process.env.SMTP_PORT || '2525'),
       secure: false,
       auth: {
         user: process.env.SMTP_EMAIL,
